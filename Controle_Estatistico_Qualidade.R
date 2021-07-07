cd0e81279ebfa8bcf85edc6720d46a0d0e4d3eaf
MarioDhiego <mario.valente@detran.pa.gov.br>
2021-07-06 21:59:57 -0300

    Atualização de script

--- Controle_Estatistico_Qualidade.R
+++ Controle_Estatistico_Qualidade.R
@@ -1,13 +1,30 @@
 
-################# Controle Estatistico da Qualidade ###################################### 
+
+# Apresentando um Exemplo e Aplicações Básica de CEQ na
+# Linguagem de Programação R4.1
+
+################# Controle Estatistico da Qualidade ######################################
+################# CONCEITOS BASICOS ######################################################
+
+
+# Controle Estatistico de Processo (CEP)
+# CEP: um conjunto de m?todos utilizados para planejar, monitorar e aprimorar um processo 
+# produtivo, por meio da coleta de amostras e, em seguida, da mensuracao de uma serie de
+# variaveis que reletem a qualidade do processo produtivo.
+
+
+
+
+
+
 ################# 7 Ferramentas da Qualidade #############################################
-# 1) Fluxograma (ou Estratificação)
+# 1) Fluxograma (ou Estratificacao)
 # 2) Diagrama de Ishikawa
-# 3) Folhas de Verificação (check sheet)
+# 3) Folhas de Verificacao (check sheet)
 # 4) Diagrama de Pareto
 # 5) Histograma
-# 6) Disgrama de Dispersão
-# 7) Gráficos de Controle
+# 6) Diagrama de Dispersao
+# 7) Grdficos de Controle
 ##########################################################################################
 
 
@@ -37,25 +54,25 @@ library(spc)
 # VANTAGENS
 # 1) Separa as causas dos efeitos
 # 2) Identifica várias causas para o mesmo efeito
-# 3) Visualização clara das causas possíveis para um mesmo efeito
+# 3) Visualização clara das causas possiveis para um mesmo efeito
 
 
-### Usando o pacote qcc (“Quality control chart”)
+### Usando o pacote qcc (Quality control chart)
 # Argumentos do Pacote
 # 1) cause     : uma lista de causas e ramos fornecendo rótulos descritivos. 
 # 2) effect    : um rótulo de string ou o efeito.
-# 3) title	   : uma string especificando o título principal para aparecer no diagrama  
+# 3) title	   : uma string especificando o titulo principal para aparecer no diagrama  
 # 4) cex       : um vetor de valores para a expansão gráfica do personagem. Os valores referem-se, pela ordem, aos RAMOS, CAUSA e EFEITOS.
 # 5) font      : um vetor de valores para a fonte, os valores referem-se, pela ordem, aos ramos, causas e efeitos.
 
 ###################################################################################################
-cause.and.effect(cause = list(Medida   = c("Aferição","Acompanhamento","Indicador"),
-                              Material = c("Qualidade","Padronização","Entrega"),
-                              Pessoas  = c("Comunicação","Disciplina","Criatividade"),
-                              Ambiente = c("Calor","Frio","Espaço"),
+cause.and.effect(cause = list(Medida   = c("Afericao","Acompanhamento","Indicador"),
+                              Material = c("Qualidade","Padronizacao","Entrega"),
+                              Pessoas  = c("Comunicacao","Disciplina","Criatividade"),
+                              Ambiente = c("Calor","Frio","Espaco"),
                               Metodo   = c("Procedimentos","Etapas","Manuais"),
                               Maquina  = c("Equipamentos","Ferramentas","Instrumentos")),
-                 effect = c("Redução/Aumento/Nº de Defeitos"),
+                 effect = c("Reducao/Aumento/N? de Defeitos"),
                  title  = "Diagrama de Causa e Efeito", 
                  cex    = c(1,1,1), 
                  font  = c(2,2,2)) 
@@ -68,17 +85,17 @@ cause.and.effect(cause = list(Medida   = c("Aferição","Acompanhamento","Indica
 # 1) effect    : Uma sequência de caracteres curta que representa o efeito que desejamos analisar.
 # 2) causes.gr : Um vetor de caracteres que representa os grupos de causas.
 # 3) causes	   : Um vetor com listas que representam as causas individuais para cada
-# 4) main	     : Título principal do diagrama
-# 5) sub	     : Subtítulo para o diagrama (recomendado o nome do projeto Six Sigma)
+# 4) main	     : Titulo principal do diagrama
+# 5) sub	     : Subtitulo para o diagrama (recomendado o nome do projeto Six Sigma)
 # 6) ss.col	   : Um vetor de cores para um desenho personalizado. Pelo menos cinco cores, classificadas por intensidade descendente
 
 ###########################################################################################
 Efeito <- "ALTA DENSIDADE"
 
-Causas_Primárias <- c("MÉTODOS", "MÁQUINAS", "MÃO DE OBRA", 
+Causas_Primarias <- c("MATODOS", "MAQUINAS", "MAO DE OBRA", 
                       "MATERIAL", "MEDIDAS", "MEIO AMBIENTE")
 
-Causas_Secundárias <- vector(mode = "list", length = length(Causas_Primárias))
+Causas_Secundarias <- vector(mode = "list", length = length(Causas_Primarias))
 
 Causas_Secundárias[1] <- list(c("Recpção de Materias-Primas", "Metodo de Transporte"))
 Causas_Secundárias[2] <- list(c("Compressor", "Condições Operacionais", "Regulagem da Maquina"))
@@ -95,12 +112,9 @@ Ishikawa_Densidade <- ss.ceDiag(Efeito, Causas_Primárias, Causas_Secundárias,
 
 
 ############### Diagrama de Pareto #######################################################
-#
-# Vilfredo Pareto (1848-1923), engenheiro e economista italiano
+#### Vilfredo Pareto (1848-1923), engenheiro e economista italiano
 
-
-# Ex: Observação da Linha de Produção de Tecidos
-
+#### Ex: Observacao da Linha de Producao de Tecidos
 # 1) Tipos de Não Conformidades
 #   i)   Riscos
 #   ii)  Manchas
@@ -108,7 +122,7 @@ Ishikawa_Densidade <- ss.ceDiag(Efeito, Causas_Primárias, Causas_Secundárias,
 #   iv)  Furos
 #   vi)  Rasgos
 
-# 2) Número de Não Conformidades
+# 2) Numero de Nao Conformidades
 # 3) Custo de Retrabalho p/ Unidades
 # 4) Custo Total de Retrabalho
 
@@ -134,55 +148,55 @@ pareto.chart(retrabalho,
 ###################################################################################################
 
 
-################ Folha de Verificação ############################################################
+################ Folha de Verificacao ############################################################
 
 # Pra que Serve?
-# 1) Tabela para coletar e resumir informações
-# 2) Contagem de ocorrência de eventos: ocorrências, consultas, falhas,defeitos, não-conformidades, etc
-# 3) Fácil utilização, visualização e interpretação
+# 1) Tabela para coletar e resumir informacoes
+# 2) Contagem de ocorrencia de eventos: ocorrencias, consultas, falhas,defeitos, nao-conformidades, etc
+# 3) Facil utilizacao, visualizacao e interpretacao
 # 4) Coleta de dados de forma padronizada
-# 5) É um Registro da Qualidade, portanto deve ser devidamente identificada, datada, e assinada por responsável
+# 5) E um Registro da Qualidade, portanto deve ser devidamente identificada, datada, e assinada por responsável
 
 
-# Passos para Aplicação
-# 1) Escolher um tipo de folha de verificação que permite um fácil preenchimento, organizar conforme a necessidade da organização ou setor, no qual será feita a coleta de dados.
-# 2) Definir a quantidade de informações irá conter no problema/falhas ou ocorrência e o tamanho da amostra dos dados
+# Passos para Aplicacao
+# 1) Escolher um tipo de folha de verificacao que permite um facil preenchimento, organizar conforme a necessidade da organização ou setor, no qual será feita a coleta de dados.
+# 2) Definir a quantidade de informacoes ira conter no problema/falhas ou ocorrencia e o tamanho da amostra dos dados
 # 3) Decidir o local ou setor onde será a coleta de dados
 # 4) Determinar a frequência com que serão coletados os dados, pode ser diário, semanal ou mensal. Serve para verificar a ocorrência durante um determinado período
 # 5) Definir quem deverá coletar os dados, poderá ser o gestor do setor que está sendo coletado os dados ou supervisor direto do setor operacional;
-# 6) Aplicar a folha de verificação atingir a coleta dentro do planejado.
+# 6) Aplicar a folha de verificacao atingir a coleta dentro do planejado.
 
 
-# Fazer uma Folha de Verificação
-folha_verificação <- rbind(data.frame(Group = "MÉTODOS", cause = Métodos),
-                           data.frame(Group = "MÁQUINAS", cause = Máquinas),
-                           data.frame(Group = "MÃO DE OBRA", cause = Mãodeobra),
+# Fazer uma Folha de Verificacao
+folha_verificacao <- rbind(data.frame(Group = "MATODOS", cause = Matodos),
+                           data.frame(Group = "MAQUINAS", cause = Maquinas),
+                           data.frame(Group = "MAO DE OBRA", cause = Maodeobra),
                            data.frame(Group = "MATERIAL", cause = Material),
                            data.frame(Group = "MEDIDAS", cause = Medidas),
                            data.frame(Group = "MEIO AMBIENTE", cause = Meioambiente))
 
-folha_verificação$Inspetor_A <- NA 
-folha_verificação$Inspetor_B <- NA
-folha_verificação$Inspetor_C <- NA
+folha_verificacao$Inspetor_A <- NA 
+folha_verificacao$Inspetor_B <- NA
+folha_verificacao$Inspetor_C <- NA
 
-folha_verificação
+folha_verificacao
 
 
-# Investigação p/ Inpetores
-folha_verificação$Inspetor_A <- c(0, 0, 4, 3, 2, 0, 2, 0,
+# Investigacao p/ Inpetores
+folha_verificacao$Inspetor_A <- c(0, 0, 4, 3, 2, 0, 2, 0,
                                   2, 0, 0, 1, 1, 0, 0, 1)
 
-folha_verificação$Inspetor_B <- c(0, 0, 5, 3, 2, 0, 2, 0,
+folha_verificacao$Inspetor_B <- c(0, 0, 5, 3, 2, 0, 2, 0,
                                   2, 0, 0, 1, 1, 0, 0, 1)
 
-folha_verificação$Inspetor_C <- c(0, 0, 6, 4, 3, 0, 2, 0,
+folha_verificacao$Inspetor_C <- c(0, 0, 6, 4, 3, 0, 2, 0,
                                   2, 0, 0, 1, 1, 0, 0,1)
 
-folha_verificação$total <- folha_verificação$Inspetor_A + 
-        folha_verificação$Inspetor_B + 
-        folha_verificação$Inspetor_C
+folha_verificacao$total <- folha_verificacao$Inspetor_A + 
+        folha_verificacao$Inspetor_B + 
+        folha_verificacao$Inspetor_C
 
-folha_verificação
+folha_verificacao
 #################################################################################################
 
 
@@ -201,19 +215,19 @@ df_den_p
 plot(df_den_p, type = "fh")
 
 
-#Histograma freqência relativa
+#Histograma frequencia relativa
 plot(df_den_p, type = "rfh")
 
 
-#Histograma freqência relativa em %
+#Histograma frequencia relativa em %
 plot(df_den_p, type = "rfph")
 
 
-#Histograma freqência Acumulada
+#Histograma frequencia Acumulada
 plot(df_den_p, type = "cfh")
 
 
-#Histograma freqência Acumulada em %
+#Histograma frequencia Acumulada em %
 plot(df_den_p, type = "cfph")
 ########################################################################################
 
@@ -222,21 +236,21 @@ plot(df_den_p, type = "cfph")
 
 
 
-#################### Parâmetros do Grafico de Controle #############################################
+#################### Parametros do Grafico de Controle #############################################
 # 1) Limites de Controle
 # i)   LIC = Limite Inferior de Controle
 # ii)  LIC = Limite Superior de Controle
-# iii) μ   = Média da Amostra
-# iv)  σ   = Desvio Padrão.
-# vi)  MR  = é a média das amplitudes
-# vii) d2  = é uma constante tabelada 
+# iii)     = Media da Amostra
+# iv)      = Desvio Padrão.
+# vi)  MR  = e a media das amplitudes
+# vii) d2  = e uma constante tabelada 
 #####################################################################################################
 
-#################### Tipos de Gráficos de Controle ####################################################
+#################### Tipos de Graficos de Controle ####################################################
 
-# 1) Grafico de Controle da Média
+# 1) Grafico de Controle da Media
 # 2) Grafico de Controle da Amplitude
-# 3) Grafico de Controle da Desvio-Padrão
+# 3) Grafico de Controle da Desvio-Padrao
 # 4) Grafico de Controle da Media Movel
 # 5) Grafico de Controle da Cussum
 # 6) Grafico de Controle da Ewma
@@ -244,25 +258,25 @@ plot(df_den_p, type = "cfph")
 
 
 #################### Regra de Shewhart #########################################################
-# Regra 1: Um único ponto fora dos limites de controle
+# Regra 1: Um unico ponto fora dos limites de controle
 # Regra 2: Oito ou mais pontos seguidos acima (ou abaixo) da linha central
-# Regra 3: Seis pontos consecutivos crescendo (tendência para cima) ou descendo (tendência para baixo)
-# Regra 4: Dois de três pontos consecutivos próximos (um terço externo) de um limite de controle
-# Regra 5: Quinze pontos consecutivos próximos (um terço interno) da linha central
+# Regra 3: Seis pontos consecutivos crescendo (tendencia para cima) ou descendo (tendência para baixo)
+# Regra 4: Dois de tres pontos consecutivos proximos (um terço externo) de um limite de controle
+# Regra 5: Quinze pontos consecutivos proximos (um terço interno) da linha central
 
 ################################################################################################
 
 
 
-#################### APLICAÇÃO PRÁTICA ###############################################################
+#################### APLICACAO PRATICA ###############################################################
 
-# Densidade de Pastilas Cerâmicas
-# Um certo processo industrial produz pastilhas cerâmicas cuja densidade é uma característica 
-# crítica de qualidade de acordo com as necessidades do cliente
+# Densidade de Pastilas Ceramicas
+# Um certo processo industrial produz pastilhas ceramicas cuja densidade sao uma caracteristica 
+# critica de qualidade de acordo com as necessidades do cliente
 
 
-# As especificações técnicas atuais afirmam que a densidade de uma pastilha é considerada aceitável
-# SE for superior a 10,5 g/cm³. 
+# As especificacoes tecnicas atuais afirmam que a densidade de uma pastilha sao considerada aceitavel
+# SE for superior a 10,5 g/cm?. 
 
 
 # O que se poderia dizer sobre a qualidade do produto e o controle de processos?
@@ -272,13 +286,13 @@ densidade_pastilhas <- c(10.6817, 10.6040, 10.5709, 10.7858, 10.7668, 10.8101,
                          11.0934, 10.8530, 10.6774, 10.6712, 10.6935, 10.5669, 
                          10.8002, 10.7607, 10.5470, 10.5555, 10.5705, 10.7723)
 
-# Encontrar a Média dos valores 
+# Encontrar a Media dos valores 
 media <- mean(densidade_pastilhas)
 
-# Econtrar as Amplitudes Móveis dos valores
+# Econtrar as Amplitudes Moveis dos valores
 diff(densidade_pastilhas)
 
-# Alguns dos valores de amplitude móvel são negativos
+# Alguns dos valores de amplitude movel sao negativos
 # Calcular em Modulo
 amplitude_Modulo <- abs(diff(densidade_pastilhas))
 
@@ -295,20 +309,20 @@ LIC <- media - 3*(MR/d2)
 LSC <- media + 3*(MR/d2)
 
 
-############# Gráfico de Controle da Média Amostral #############################################################
+############# Grafico de Controle da Media Amostral #############################################################
 plot_xbar <- qcc(data = densidade_pastilhas, type="xbar", plot = T)
 
-############# Gráfico de Controle da Amplitude(R) ######################################################
+############# Grafico de Controle da Amplitude(R) ######################################################
 plot.xbar=qcc(densidade_pastilhas, type="R", plot = T)
 
-############ Gráfico de Medidas Individuais #####################################################################
+############ Grafico de Medidas Individuais #####################################################################
 plot.ind = qcc(data2, type="xbar.one")
 
-############# Gráfico de Controle do Desvio Padrão #####################################################
+############# Grafico de Controle do Desvio Padrao #####################################################
 plot.xbar=qcc(densidade_pastilhas, type="S")
 
 
-############# Gráfico de Controle de Proporção ###########################
+############# Grafico de Controle de Proporcao ###########################
 plot.xbar=qcc(densidade_pastilhas, type="p")
 
 
@@ -324,7 +338,7 @@ process.capability(object = plot.xbar,
 
 
 
-############ Atualizar o Gráfico de Controle #################################
+############ Atualizar o Grafico de Controle #################################
 
 plot.atual.1 = qcc(densidade_pastilhas[1:20,], type = "xbar", newdata = data[21:24,])
 
@@ -341,7 +355,7 @@ plot(plot.atual.2, chart.all=FALSE)
 
 
 ################### Grafico de Controle Densidade ######################################################
-# gráfico de controle para a densidade das pastilhas cerâmicas.
+# grafico de controle para a densidade das pastilhas ceramicas.
 
 plot(densidade_pastilhas,
      type = "b",
